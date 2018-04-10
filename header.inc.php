5262fbd7ec761ffe6e1162268d22cb3f0b0a28a0
Regan Thorpe <regan.c.thorpe@wmich.edu>
2018-04-10 04:29:35 -0400

    Initial Commit

---
+++ header.inc.php
@@ -0,0 +1,6 @@
+<header>
+  <h1>Ship Shop</h1>
+</header>
+<nav>
+  <h2>*Insert Nav Bar Here*</h2>
+</nav>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Our Web Store</title>
+    <link type="text/css" href="all.css" />
+    <link type="text/css" href="index.css" />
+    <link type="text/css" href="https://bootswatch.com/3/cosmo/bootstrap.min.css"/>
+  </head>
+  <body>
+    <?php include 'header.inc.php'; ?>
+    <p>
+      This is type 3 for our <a href="https://cs3500wmu.000webhostapp.com/final_project_100.php">cs3500</a> project.<br />It is tentatively a shop for ships
+    </p>
+    <p>
+      *Insert 3 wide grid here*
+    </p>
+  </body>
+</html>
