2df1b94ee113aec6c1433fd55933e0ecbd98196e
Regan Thorpe <regan.c.thorpe@wmich.edu>
2018-04-16 17:05:28 -0400

    MOAR FILEZ

--- catalog.php
+++ catalog.php
@@ -6,3 +6,12 @@ $password = "This is fine";
 $connStr = 'mysql:host=mysql.team14store.xyz;dbname=cs3500_StoreDB';
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Product List</title>
+</head>
+<body>
+
+</body>
+</html>
--- login.php
+++ login.php
@@ -1,4 +1,9 @@
-<?php  
+<?php
+session_start();
+
+$user = "team14user";
+$password = "This is fine";
+$connStr = 'mysql:host=mysql.team14store.xyz;dbname=cs3500_StoreDB';
 
 ?>
 <!DOCTYPE html>
